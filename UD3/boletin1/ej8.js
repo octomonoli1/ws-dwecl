165662ff89202e9b8b3720dc173678385d10353f
octomonoli1 <octavio.mo90@gmail.com>
2024-10-25 10:20:09 +0200

    hemos llegado al 8

---
+++ UD3/boletin1/ej8.js
@@ -0,0 +1,4 @@
+window.onload = function saludo() {
+    var parrafo = document.getElementById("parrafo").nodeValue;
+    alert(parrafo);
+};
---
+++ UD3/boletin1/ej8.ts
@@ -0,0 +1,4 @@
+window.onload = function saludo(){
+    var parrafo = (document.getElementById("parrafo") as HTMLParagraphElement).nodeValue;
+    alert(parrafo);
+}
