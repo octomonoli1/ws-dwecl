c2a38b43c9cb70d36c54d4f6b82a9bffc1b9dd57
octomonoli1 <octavio.mo90@gmail.com>
2024-10-25 10:09:36 +0200

    ejercicios 5,6,7

---
+++ UD3/boletin1/ej_5_6_7.js
@@ -0,0 +1,11 @@
+//Ejercicio 6
+setTimeout(function saluda() {
+    alert("Hola");
+}, 5000);
+//Ejercicio 7
+console.log("Nombre del navegador: " + window.navigator.appName);
+console.log("Versión del navegador: " + window.navigator.appVersion);
+//console.log("Tipo de conexion: " + window.navigator.connection);
+console.log("Plataforma del navegador" + window.navigator.platform);
+//Ejercicio 5
+window.open("https://ieslosalcores.org");
