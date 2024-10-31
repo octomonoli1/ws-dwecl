e76545ee56c63ed1aa77cdc0a80610df5c50fe99
octomonoli1 <octavio.mo90@gmail.com>
2024-10-31 09:18:39 +0100

    ejercicio 3

--- UD3/boletin2/ej2.js
+++ UD3/boletin2/ej2.js
@@ -1,7 +1,6 @@
-function mostrar_altura(){
+function mostrar_altura() {
     console.log("La altura de la ventana es: " + window.innerHeight);
 }
-
-function mostrar_anchura(){
+function mostrar_anchura() {
     console.log("La anchura de la ventana es: " + window.innerWidth);
 }
